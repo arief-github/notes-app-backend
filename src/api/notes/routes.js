d09396722092617359957e766cfc835ca2754022
github.arief <78076941+arief-github@users.noreply.github.com>
2022-07-18 09:22:23 +0700

    set up notes plugin

---
+++ src/api/notes/routes.js
@@ -0,0 +1,30 @@
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler } = require("../../handler");
+
+const routes = (handler) => [{
+        method: 'POST',
+        path: '/notes',
+        handler: handler.addNoteHandler
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: handler.getAllNotesHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: handler.getNoteByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: handler.editNoteByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: handler.deleteNoteByIdHandler
+    }
+];
+
+module.exports = routes;
