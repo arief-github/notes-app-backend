31ae956c5ca348df338ccf5c225f4d5105453c83
github.arief <78076941+arief-github@users.noreply.github.com>
2022-07-18 09:29:27 +0700

    Update server.js : registering plugins and notesService.

--- src/server.js
+++ src/server.js
@@ -1,21 +1,33 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
+const notes = require('./notes');
+const NotesService = require('./services/inMemory/NotesService');
 
-const init = async () => {
-  const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-    routes: {
-      cors: {
-        origin: ['*'],
-      },
-    },
-  });
 
-  server.route(routes);
+const init = async() => {
+    const notesService = new NotesService;
 
-  await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
+    const server = Hapi.server({
+        port: 5000,
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    });
+
+    server.route(routes);
+
+    await server.register({
+        plugin: notes,
+        options: {
+            service: notesService,
+        },
+    });
+
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
