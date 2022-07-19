0c0ac34eda0b1c9cb8e273153a4d5e027cd19e1f
github.arief <78076941+arief-github@users.noreply.github.com>
2022-07-19 09:19:08 +0700

    Adding Validator notes to POST and PUT method request

--- src/api/notes/index.js
+++ src/api/notes/index.js
@@ -1,11 +1,12 @@
+const { validator } = require("@hapi/hapi/lib/validation");
 const NotesHandler = require("./handler");
 const routes = require("./routes");
 
 module.exports = {
     name: 'notes',
     version: '1.0.0',
-    register: async(server, { service }) => {
-        const notesHandler = new NotesHandler(service);
+    register: async(server, { service, validator }) => {
+        const notesHandler = new NotesHandler(service, validator);
         server.route(routes(notesHandler));
     }
 }
